d24b162918fe39b6799e13be24d28f180836a817
Copcoptiki06 <mehmetkahriman@gmail.com>
2026-08-09 11:26:36 +0300

    Grow model lab to twenty two activities

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "modai-fen-v31";
+const CACHE = "modai-fen-v32";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
