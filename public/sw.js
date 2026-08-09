e04c385ce9d2fe8daf044cccae679b76b0ef53d4
Copcoptiki06 <mehmetkahriman@gmail.com>
2026-08-09 10:46:53 +0300

    Add grade 7 model learning labs

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "modai-fen-v27";
+const CACHE = "modai-fen-v28";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
